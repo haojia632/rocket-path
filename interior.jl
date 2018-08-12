6df4fa6af1c62e38125ad5103f04c2302015ec41
James McNeill <mcneja@gmail.com>
2018-08-11 21:45:50 -0700

    Nonlinear problem

--- interior.jl
+++ interior.jl
@@ -1,61 +1,59 @@
-# Minimize f(x0, x1) = x0
-# Constraints: x0 + x1 >= 0, x0 - x1 >= 0
-# State vector is x0, x1, c0, c1
-# c0, c1 are the constraint multipliers
-
 using LinearAlgebra: Diagonal
 
-objectiveGradient(x) = Float64[1, 0]
+# Objective function gradient and Hessian
 
-constraint_1_error(x) = x[1] + x[2]
-constraint_1_gradients(x) = Float64[1, 1]
+obj_grad(x) = Float64[1, 0]
+obj_hess(x) = Float64[0 0; 0 0]
 
-constraint_2_error(x) = x[1] - x[2]
-constraint_2_gradients(x) = Float64[1, -1]
+# Constraint errors, gradients, and Hessians
 
-constraint_3_error(x) = 10 - x[1]
-constraint_3_gradients(x) = Float64[-1, 0]
+c1_err(x) = 1 - 0.5*(x[1]^2 + x[2]^2)
+c1_grad(x) = Float64[-x[1], -x[2]]
+c1_hess(x) = Float64[-1 0; 0 -1]
 
-constraint_errors(x) = Float64[
-	constraint_1_error(x),
-	constraint_2_error(x),
-	constraint_3_error(x)
-]
+c2_err(x) = x[1] + x[2]
+c2_grad(x) = Float64[1, 1]
+c2_hess(x) = zeros(2, 2)
 
-constraint_gradients(x) = vcat(
-	transpose(constraint_1_gradients(x)),
-	transpose(constraint_2_gradients(x)),
-	transpose(constraint_3_gradients(x))
-)
+# Constraint collection
 
-function residuals(x)
+c_err = [c1_err, c2_err]
+c_grad = [c1_grad, c2_grad]
+c_hess = [c1_hess, c2_hess]
+
+# Helper functions
+
+c_errs(x) = [f(x) for f=c_err]
+c_grads(x) = vcat([transpose(f(x)) for f=c_grad]...)
+c_hesss(x) = [f(x) for f=c_hess]
+
+function resid(x)
 	y = x[3:end] # constraint multipliers
 	vcat(
-		objectiveGradient(x) + transpose(constraint_gradients(x)) * y,
-		-(y .* constraint_errors(x))
+		obj_grad(x) + transpose(c_grads(x)) * y,
+		-(y .* c_errs(x))
 	)
 end
 
-function stepMatrix(x)
+function step_mat(x)
 	y = x[3:end] # constraint multipliers
+	hess = obj_hess(x) + sum(y .* c_hesss(x))
 	vcat(
-		hcat(zeros(2, 2), transpose(constraint_gradients(x))),
-		hcat(-(y .* constraint_gradients(x)), -Array(Diagonal(constraint_errors(x))))
+		hcat(hess, transpose(c_grads(x))),
+		hcat(-(y .* c_grads(x)), -Array(Diagonal(c_errs(x))))
 	)
 end
 
-barrier(t) = Float64[0, 0, 1/t, 1/t, 1/t]
+barrier(u) = Float64[0, 0, u, u]
 
-step(x, t) = x + stepMatrix(x) \ (barrier(t) - residuals(x))
+step(x, u) = x + step_mat(x) \ (barrier(u) - resid(x))
 
-x = Float64[-2, 1, 1, 1, 1]
-@show x
-@show residuals(x)
+x = Float64[0, 0, -0.5, -0.5]
+# x = Float64[0.1, 0, -0.5, -0.5]
+@show x, resid(x)
 
 for i = 1:16
-	t = 2^i
-	global x = step(x, t)
-	@show x
+	u = 1/2^i
+	global x = step(x, u)
+	@show x, resid(x)
 end
-
-@show residuals(x)
